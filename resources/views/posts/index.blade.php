f0599a4ffccbad89db0099b6160b734760612602
Osuda <fuuneko4972@gmail.com>
2021-12-24 03:49:30 +0900

    08-6

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+       <p class="create">[<a href='/posts/create'>create</a>]</p>
+<form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post" style="display:inline">
+    @csrf
+    @method('DELETE')
+    <button type="submit">delete</button> 
+</form>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                   <h2 class='title'>
+    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
+    </body>
+</html>
